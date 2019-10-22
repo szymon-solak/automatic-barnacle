3f0c673a7f720ac556cb41044b1610a79980aab8
Szymon Solak <szymon.solak@brainhub.pl>
2019-10-22 19:59:59 +0200

    Add example repo method and mapping

--- packages/server/src/domain/models/user.ts
+++ packages/server/src/domain/models/user.ts
@@ -4,8 +4,24 @@ import { Option } from '../../types/option'
 
 export type UserId = boundType<Id, 'UserId'>
 
+export interface UserRow {
+  id: Id
+  displayName: string
+  photoUrl?: string
+}
+
 export interface User extends Entity<UserId> {
   displayName: string
   photoUrl: Option<string>
 }
 
+const mapToUserId = (id: Id): UserId => {
+  return (id as unknown) as UserId
+}
+
+export const mapUserToModel = (row: Option<UserRow>): Option<User> =>
+  row.map(user => ({
+    id: mapToUserId(user.id),
+    displayName: user.displayName,
+    photoUrl: Option.of(user.photoUrl),
+  }))
--- packages/server/src/repositories/user-repository.ts
+++ packages/server/src/repositories/user-repository.ts
@@ -1,5 +1,17 @@
 import { DatabaseConnection } from '../config/db'
+import { UserId, UserRow, mapUserToModel } from '../domain/models/user'
 
 export class UserRepository {
   constructor(private readonly databaseConnection: DatabaseConnection) {}
+
+  public async getById(id: UserId) {
+    const result = await this.databaseConnection.query<UserRow>(
+      'SELECT FROM users WHERE id = $1',
+      [id.toString()]
+    )
+
+    const user = result.map(x => x[0])
+
+    return mapUserToModel(user)
+  }
 }
